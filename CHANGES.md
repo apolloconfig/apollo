0745881ec15cfd951099f0305f76157810471d53
JaredTan95 <jian.tan@daocloud.io>
2021-08-26 11:31:51 +0800

    update org content.

--- CHANGES.md
+++ CHANGES.md
@@ -6,7 +6,7 @@ Apollo 1.10.0
 
 ------------------
 * [Bump version to 1.10.0](https://github.com/ctripcorp/apollo/pull/3917)
+* [remove ctrip profile](https://github.com/ctripcorp/apollo/pull/3918)
 
 ------------------
 All issues and pull requests are [here](https://github.com/ctripcorp/apollo/milestone/8?closed=1)
-
--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/spi/configuration/MQConfiguration.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/spi/configuration/MQConfiguration.java
@@ -14,40 +14,23 @@
  * limitations under the License.
  *
  */
+
 package com.ctrip.framework.apollo.portal.spi.configuration;
 
-import com.ctrip.framework.apollo.common.condition.ConditionalOnMissingProfile;
-import com.ctrip.framework.apollo.portal.spi.ctrip.CtripMQService;
 import com.ctrip.framework.apollo.portal.spi.defaultimpl.DefaultMQService;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MQConfiguration {
 
-  @Configuration
-  @Profile("ctrip")
-  public static class CtripMQConfiguration {
+    @Configuration
+    public static class DefaultMQConfiguration {
 
-    @Bean
-    public CtripMQService mqService() {
-      return new CtripMQService();
+        @Bean
+        public DefaultMQService mqService() {
+            return new DefaultMQService();
+        }
     }
-  }
-
-  /**
-   * spring.profiles.active != ctrip
-   */
-  @Configuration
-  @ConditionalOnMissingProfile({"ctrip"})
-  public static class DefaultMQConfiguration {
-
-    @Bean
-    public DefaultMQService mqService() {
-      return new DefaultMQService();
-    }
-  }
 
 }
