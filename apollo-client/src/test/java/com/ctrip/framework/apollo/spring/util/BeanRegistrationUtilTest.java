45f3bbfb5d8b79f5f517b38c40157ad6a5db1c6d
ayushjha <ayush0407@hotmail.com>
2022-03-20 12:52:22 +0530

    Adding copyright comment. Fixes #3874.

--- apollo-client/src/test/java/com/ctrip/framework/apollo/spring/util/BeanRegistrationUtilTest.java
+++ apollo-client/src/test/java/com/ctrip/framework/apollo/spring/util/BeanRegistrationUtilTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Apollo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.ctrip.framework.apollo.spring.util;
 
 import static org.junit.Assert.assertFalse;
@@ -17,7 +33,7 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.SimpleBeanDefinitionRegistry;
 
 /**
- * @author Ayush Jha 
+ * @author Ayush Jha
  */
 
 @RunWith(MockitoJUnitRunner.class)
@@ -26,7 +42,7 @@ public class BeanRegistrationUtilTest {
 	@InjectMocks
 	private BeanRegistrationUtil beanRegistrationUtil;
 	private BeanDefinitionRegistry someRegistry;
-	String someBeanName = "someBean";
+	private String someBeanName = "someBean";
 
 	@Before
 	public void setUp() {
