604244d00feca2853205c46512719d68aff4f57a
dlorgine <344649710@qq.com>
2022-05-22 17:16:28 +0800

    优化cat 监控

--- apollo-core/src/main/java/com/ctrip/framework/apollo/tracer/internals/cat/CatMessageProducer.java
+++ apollo-core/src/main/java/com/ctrip/framework/apollo/tracer/internals/cat/CatMessageProducer.java
@@ -18,44 +18,16 @@ package com.ctrip.framework.apollo.tracer.internals.cat;
 
 import com.ctrip.framework.apollo.tracer.spi.MessageProducer;
 import com.ctrip.framework.apollo.tracer.spi.Transaction;
-
-import java.lang.reflect.Method;
+import com.dianping.cat.Cat;
 
 /**
  * @author Jason Song(song_s@ctrip.com)
  */
 public class CatMessageProducer implements MessageProducer {
-  private static Class CAT_CLASS;
-  private static Method LOG_ERROR_WITH_CAUSE;
-  private static Method LOG_ERROR_WITH_MESSAGE_AND_CAUSE;
-  private static Method LOG_EVENT_WITH_TYPE_AND_NAME;
-  private static Method LOG_EVENT_WITH_TYPE_AND_NAME_AND_STATUS_AND_NAME_VALUE_PAIRS;
-  private static Method NEW_TRANSACTION_WITH_TYPE_AND_NAME;
-
-  static {
-    try {
-      CAT_CLASS = Class.forName(CatNames.CAT_CLASS);
-      LOG_ERROR_WITH_CAUSE = CAT_CLASS.getMethod(CatNames.LOG_ERROR_METHOD, Throwable.class);
-      LOG_ERROR_WITH_MESSAGE_AND_CAUSE = CAT_CLASS.getMethod(CatNames.LOG_ERROR_METHOD,
-          String.class, Throwable.class);
-      LOG_EVENT_WITH_TYPE_AND_NAME = CAT_CLASS.getMethod(CatNames.LOG_EVENT_METHOD,
-          String.class, String.class);
-      LOG_EVENT_WITH_TYPE_AND_NAME_AND_STATUS_AND_NAME_VALUE_PAIRS =
-          CAT_CLASS.getMethod(CatNames.LOG_EVENT_METHOD, String.class, String.class,
-              String.class, String.class);
-      NEW_TRANSACTION_WITH_TYPE_AND_NAME = CAT_CLASS.getMethod(
-          CatNames.NEW_TRANSACTION_METHOD, String.class, String.class);
-      //eager init CatTransaction
-      CatTransaction.init();
-    } catch (Throwable ex) {
-      throw new IllegalStateException("Initialize Cat message producer failed", ex);
-    }
-  }
-
   @Override
   public void logError(Throwable cause) {
     try {
-      LOG_ERROR_WITH_CAUSE.invoke(null, cause);
+      Cat.logError(cause);
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
     }
@@ -64,7 +36,7 @@ public class CatMessageProducer implements MessageProducer {
   @Override
   public void logError(String message, Throwable cause) {
     try {
-      LOG_ERROR_WITH_MESSAGE_AND_CAUSE.invoke(null, message, cause);
+      Cat.logError( message, cause);
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
     }
@@ -73,7 +45,7 @@ public class CatMessageProducer implements MessageProducer {
   @Override
   public void logEvent(String type, String name) {
     try {
-      LOG_EVENT_WITH_TYPE_AND_NAME.invoke(null, type, name);
+      Cat.logEvent(type, name);
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
     }
@@ -82,7 +54,7 @@ public class CatMessageProducer implements MessageProducer {
   @Override
   public void logEvent(String type, String name, String status, String nameValuePairs) {
     try {
-      LOG_EVENT_WITH_TYPE_AND_NAME_AND_STATUS_AND_NAME_VALUE_PAIRS.invoke(null, type, name,
+      Cat.logEvent(type, name,
           status, nameValuePairs);
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
@@ -92,7 +64,7 @@ public class CatMessageProducer implements MessageProducer {
   @Override
   public Transaction newTransaction(String type, String name) {
     try {
-      return new CatTransaction(NEW_TRANSACTION_WITH_TYPE_AND_NAME.invoke(null, type, name));
+      return new CatTransaction(Cat.newTransaction(type, name));
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
     }
--- apollo-core/src/main/java/com/ctrip/framework/apollo/tracer/internals/cat/CatTransaction.java
+++ apollo-core/src/main/java/com/ctrip/framework/apollo/tracer/internals/cat/CatTransaction.java
@@ -24,40 +24,17 @@ import java.lang.reflect.Method;
  * @author Jason Song(song_s@ctrip.com)
  */
 public class CatTransaction implements Transaction {
-  private static Class CAT_TRANSACTION_CLASS;
-  private static Method SET_STATUS_WITH_STRING;
-  private static Method SET_STATUS_WITH_THROWABLE;
-  private static Method ADD_DATA_WITH_KEY_AND_VALUE;
-  private static Method COMPLETE;
 
-  private Object catTransaction;
+  private com.dianping.cat.message.Transaction catTransaction;
 
-  static {
-    try {
-      CAT_TRANSACTION_CLASS = Class.forName(CatNames.CAT_TRANSACTION_CLASS);
-      SET_STATUS_WITH_STRING = CAT_TRANSACTION_CLASS.getMethod(CatNames.SET_STATUS_METHOD, String.class);
-      SET_STATUS_WITH_THROWABLE = CAT_TRANSACTION_CLASS.getMethod(CatNames.SET_STATUS_METHOD,
-          Throwable.class);
-      ADD_DATA_WITH_KEY_AND_VALUE = CAT_TRANSACTION_CLASS.getMethod(CatNames.ADD_DATA_METHOD,
-          String.class, Object.class);
-      COMPLETE = CAT_TRANSACTION_CLASS.getMethod(CatNames.COMPLETE_METHOD);
-    } catch (Throwable ex) {
-      throw new IllegalStateException("Initialize Cat transaction failed", ex);
-    }
-  }
-
-  static void init() {
-    //do nothing, just to initialize the static variables
-  }
-
-  public CatTransaction(Object catTransaction) {
+  public CatTransaction(com.dianping.cat.message.Transaction catTransaction) {
     this.catTransaction = catTransaction;
   }
 
   @Override
   public void setStatus(String status) {
     try {
-      SET_STATUS_WITH_STRING.invoke(catTransaction, status);
+      catTransaction.setStatus(status);
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
     }
@@ -66,7 +43,7 @@ public class CatTransaction implements Transaction {
   @Override
   public void setStatus(Throwable status) {
     try {
-      SET_STATUS_WITH_THROWABLE.invoke(catTransaction, status);
+      catTransaction.setStatus(status);
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
     }
@@ -75,7 +52,7 @@ public class CatTransaction implements Transaction {
   @Override
   public void addData(String key, Object value) {
     try {
-      ADD_DATA_WITH_KEY_AND_VALUE.invoke(catTransaction, key, value);
+      catTransaction.addData(key, value);
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
     }
@@ -84,7 +61,7 @@ public class CatTransaction implements Transaction {
   @Override
   public void complete() {
     try {
-      COMPLETE.invoke(catTransaction);
+      catTransaction.complete();
     } catch (Throwable ex) {
       throw new IllegalStateException(ex);
     }
