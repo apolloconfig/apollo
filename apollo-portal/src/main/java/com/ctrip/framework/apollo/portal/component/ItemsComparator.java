92c559ad4b667da4c85224fac12d673972a3334b
furaul <furaul@qq.com>
2022-09-17 21:57:34 +0800

    Fix 'Type missing problem' while click 'Full Release' in Grayscale Version

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/component/ItemsComparator.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/component/ItemsComparator.java
@@ -53,6 +53,7 @@ public class ItemsComparator {
       }else if (!Objects.equals(sourceItem.getValue(), item.getValue())){//update
         //only value & comment can be update
         sourceItem.setValue(item.getValue());
+        sourceItem.setType(item.getType());
         sourceItem.setComment(item.getComment());
         changeSets.addUpdateItem(sourceItem);
       }
@@ -90,6 +91,7 @@ public class ItemsComparator {
   private ItemDTO copyItem(ItemDTO sourceItem){
     ItemDTO copiedItem = new ItemDTO();
     copiedItem.setKey(sourceItem.getKey());
+    copiedItem.setType(sourceItem.getType());
     copiedItem.setValue(sourceItem.getValue());
     copiedItem.setComment(sourceItem.getComment());
     return copiedItem;
