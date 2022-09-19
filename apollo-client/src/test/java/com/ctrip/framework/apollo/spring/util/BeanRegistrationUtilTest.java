9e8e8fac17dccbe7e6dc9c4b2c50e8141ad88b45
liaozan <378024053@qq.com>
2022-09-20 00:29:08 +0800

    Add unit test in BeanRegistrationUtilTest

--- apollo-client/src/test/java/com/ctrip/framework/apollo/spring/util/BeanRegistrationUtilTest.java
+++ apollo-client/src/test/java/com/ctrip/framework/apollo/spring/util/BeanRegistrationUtilTest.java
@@ -50,7 +50,6 @@ public class BeanRegistrationUtilTest {
         getClass(), null));
     assertFalse(BeanRegistrationUtil.registerBeanDefinitionIfNotExists(someRegistry, someBeanName,
         getClass()));
-
   }
 
   @Test
@@ -58,7 +57,6 @@ public class BeanRegistrationUtilTest {
     someRegistry.registerBeanDefinition("someAnotherBean", Mockito.mock(BeanDefinition.class));
     assertTrue(BeanRegistrationUtil.registerBeanDefinitionIfNotExists(someRegistry, someBeanName,
         getClass(), null));
-
   }
 
   @Test
@@ -68,7 +66,12 @@ public class BeanRegistrationUtilTest {
     extraPropertyValues.put(someBeanName, "someProperty");
     assertTrue(BeanRegistrationUtil.registerBeanDefinitionIfNotExists(someRegistry, someBeanName,
         getClass(), extraPropertyValues));
+  }
 
+  @Test
+  public void registerBeanDefinitionIfNotExistsWithoutBeanNameTest() {
+    someRegistry.registerBeanDefinition(BeanDefinition.class.getName(), Mockito.mock(BeanDefinition.class));
+    assertFalse(BeanRegistrationUtil.registerBeanDefinitionIfNotExists(someRegistry, BeanDefinition.class));
   }
 
 }
