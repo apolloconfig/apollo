46df1c0c965ef59c968254c08c3053d5bbe4e14a
Anilople <anilople@outlook.com>
2022-10-02 22:43:59 +0800

    fix: test fail when missing bean RegistryService

--- apollo-adminservice/src/main/java/com/ctrip/framework/apollo/adminservice/controller/RegistryController.java
+++ apollo-adminservice/src/main/java/com/ctrip/framework/apollo/adminservice/controller/RegistryController.java
@@ -21,12 +21,14 @@ import com.ctrip.framework.apollo.biz.service.RegistryService;
 import java.time.Duration;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/registry")
+@ConditionalOnBean(RegistryService.class)
 public class RegistryController {
 
   @Autowired
