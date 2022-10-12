c32f3ab936b13aa15dc4539097a021793a0ca58b
webSue <sutong1224@gmail.com>
2022-10-13 00:15:23 +0800

    update test

--- apollo-portal/src/test/java/com/ctrip/framework/apollo/portal/controller/ServerConfigControllerTest.java
+++ apollo-portal/src/test/java/com/ctrip/framework/apollo/portal/controller/ServerConfigControllerTest.java
@@ -16,16 +16,30 @@
  */
 package com.ctrip.framework.apollo.portal.controller;
 
+import com.ctrip.framework.apollo.common.dto.AppDTO;
 import com.ctrip.framework.apollo.portal.AbstractIntegrationTest;
 import com.ctrip.framework.apollo.portal.entity.po.ServerConfig;
+import com.ctrip.framework.apollo.portal.repository.ServerConfigRepository;
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by kezhenxu at 2019/1/14 13:24.
@@ -34,6 +48,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 @ActiveProfiles("skipAuthorization")
 public class ServerConfigControllerTest extends AbstractIntegrationTest {
+  @Mock
+  private ServerConfigRepository serverConfigRepository;
+  @Mock
+  private ServerConfigController serverConfigController;
+
+  private static final Gson GSON = new Gson();
   @Test
   public void shouldSuccessWhenParameterValid() {
     ServerConfig serverConfig = new ServerConfig();
@@ -76,4 +96,21 @@ public class ServerConfigControllerTest extends AbstractIntegrationTest {
       );
     }
   }
+
+  @Test
+  public void testCreateOrUpdateConfig() {
+    ServerConfig serverConfig = new ServerConfig();
+    serverConfig.setKey("someKey");
+    serverConfig.setValue("someValue");
+
+    serverConfigController.createOrUpdate(serverConfig);
+  }
+  @Test
+  public void testFindEmpty() {
+    when(serverConfigRepository.findAll()).thenReturn(new ArrayList<>());
+    List<ServerConfig> serverConfigs = serverConfigController.findAllServerConfig(0,10);
+    Assert.assertNotNull(serverConfigs);
+    Assert.assertEquals(0,serverConfigs.size());
+  }
+
 }
