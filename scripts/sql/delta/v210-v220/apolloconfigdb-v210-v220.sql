5bb73d1795e3ae96310a933044b9283d28938e9d
wxq <Anilople@outlook.com>
2022-10-14 21:09:14 +0800

    Update scripts/sql/delta/v210-v220/apolloconfigdb-v210-v220.sql
    Co-authored-by: Jason Song <nobodyiam@gmail.com>

--- scripts/sql/delta/v210-v220/apolloconfigdb-v210-v220.sql
+++ scripts/sql/delta/v210-v220/apolloconfigdb-v210-v220.sql
@@ -18,13 +18,13 @@
 Use ApolloConfigDB;
 
 CREATE TABLE `ServiceRegistry` (
-	`Id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增Id',
-	`ServiceName` VARCHAR(64) NOT NULL COMMENT '服务名',
-	`Uri` VARCHAR(64) NOT NULL COMMENT '服务地址',
-	`Cluster` VARCHAR(64) NOT NULL COMMENT '集群，可以用来标识apollo.cluster或者网络分区',
-	`DataChange_CreatedTime` TIMESTAMP NOT NULL COMMENT '创建时间',
-	`DataChange_LastTime` TIMESTAMP NOT NULL COMMENT '最后修改时间',
-	PRIMARY KEY (`Id`),
-	UNIQUE INDEX `IX_UNIQUE_KEY` (`ServiceName`, `Uri`) USING BTREE,
-	INDEX `IX_DataChange_LastTime` (`DataChange_LastTime`) USING BTREE
+  `Id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增Id',
+  `ServiceName` VARCHAR(64) NOT NULL COMMENT '服务名',
+  `Uri` VARCHAR(64) NOT NULL COMMENT '服务地址',
+  `Cluster` VARCHAR(64) NOT NULL COMMENT '集群，可以用来标识apollo.cluster或者网络分区',
+  `DataChange_CreatedTime` TIMESTAMP NOT NULL COMMENT '创建时间',
+  `DataChange_LastTime` TIMESTAMP NOT NULL COMMENT '最后修改时间',
+  PRIMARY KEY (`Id`),
+  UNIQUE INDEX `IX_UNIQUE_KEY` (`ServiceName`, `Uri`)
+  INDEX `IX_DataChange_LastTime` (`DataChange_LastTime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='注册中心';
