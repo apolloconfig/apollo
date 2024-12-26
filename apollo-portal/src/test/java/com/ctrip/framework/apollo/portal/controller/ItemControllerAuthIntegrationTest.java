e77f9a08d945db09e123775553c543ff625f0033
luke <348358584@qq.com>
2024-12-26 10:47:59 +0800

    feat(auth): add Licenses

--- apollo-portal/src/test/java/com/ctrip/framework/apollo/portal/controller/ItemControllerAuthIntegrationTest.java
+++ apollo-portal/src/test/java/com/ctrip/framework/apollo/portal/controller/ItemControllerAuthIntegrationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Apollo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.ctrip.framework.apollo.portal.controller;
 
 import static org.junit.Assert.assertEquals;
@@ -7,7 +23,7 @@ import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.ctrip.framework.apollo.AuthorizationConfiguration;
+import com.ctrip.framework.apollo.ControllableAuthorizationConfiguration;
 import com.ctrip.framework.apollo.common.dto.ItemDTO;
 import com.ctrip.framework.apollo.portal.PortalApplication;
 import com.ctrip.framework.apollo.portal.entity.bo.UserInfo;
@@ -34,7 +50,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {PortalApplication.class,
-    AuthorizationConfiguration.class}, webEnvironment = WebEnvironment.RANDOM_PORT)
+    ControllableAuthorizationConfiguration.class}, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ItemControllerAuthIntegrationTest {
 
   private final Gson GSON = new Gson();
