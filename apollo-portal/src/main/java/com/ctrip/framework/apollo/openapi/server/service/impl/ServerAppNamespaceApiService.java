7c2f895d3bf3e5bd1d1fea882fd5ac1ead9e0daf
tacklequestions <marveloussweetie@gmail.com>
2025-09-23 18:28:54 +0800

    fix AppNamespaceOpenApiService license issue

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/openapi/server/service/impl/ServerAppNamespaceApiService.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/openapi/server/service/impl/ServerAppNamespaceApiService.java
@@ -1,27 +1,42 @@
-package com.ctrip.framework.apollo.openapi.server.service;
+/*
+ * Copyright 2024 Apollo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ctrip.framework.apollo.openapi.server.service.impl;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.ctrip.framework.apollo.openapi.server.service.AppNamespaceOpenApiService;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import com.ctrip.framework.apollo.common.entity.AppNamespace;
-import com.ctrip.framework.apollo.openapi.api.AppNamespaceOpenApiService;
-import com.ctrip.framework.apollo.openapi.dto.OpenAppNamespaceDTO;
+import com.ctrip.framework.apollo.openapi.model.OpenAppNamespaceDTO;
 import com.ctrip.framework.apollo.openapi.util.OpenApiBeanUtils;
 import com.ctrip.framework.apollo.portal.listener.AppNamespaceCreationEvent;
 import com.ctrip.framework.apollo.portal.service.AppNamespaceService;
 import com.ctrip.framework.apollo.portal.spi.UserInfoHolder;
 
 @Service
-public class ServerAppNamesapceApiService implements AppNamespaceOpenApiService{
+public class ServerAppNamespaceApiService implements AppNamespaceOpenApiService {
 
     private final AppNamespaceService appNamespaceService;
     private final UserInfoHolder userInfoHolder;
     private final ApplicationEventPublisher publisher;
 
-    public ServerAppNamesapceApiService(
+    public ServerAppNamespaceApiService(
         AppNamespaceService appNamespaceService,
         UserInfoHolder userInfoHolder,
         ApplicationEventPublisher publisher) {
