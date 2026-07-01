e78e6a362379a0774a1acf8d65d100e5541635c8
Yike Xiao <kmter@live.com>
2026-07-01 11:15:53 +0800

    fix: enforce access key for raw /configfiles api

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/util/AccessKeyUtil.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/util/AccessKeyUtil.java
@@ -33,6 +33,7 @@ public class AccessKeyUtil {
   private static final String URL_SEPARATOR = "/";
   private static final String URL_CONFIGS_PREFIX = "/configs/";
   private static final String URL_CONFIGFILES_JSON_PREFIX = "/configfiles/json/";
+  private static final String URL_CONFIGFILES_RAW_PREFIX = "/configfiles/raw/";
   private static final String URL_CONFIGFILES_PREFIX = "/configfiles/";
   private static final String URL_NOTIFICATIONS_PREFIX = "/notifications/v2";
 
@@ -58,6 +59,8 @@ public class AccessKeyUtil {
       appId = StringUtils.substringBetween(servletPath, URL_CONFIGS_PREFIX, URL_SEPARATOR);
     } else if (StringUtils.startsWith(servletPath, URL_CONFIGFILES_JSON_PREFIX)) {
       appId = StringUtils.substringBetween(servletPath, URL_CONFIGFILES_JSON_PREFIX, URL_SEPARATOR);
+    } else if (StringUtils.startsWith(servletPath, URL_CONFIGFILES_RAW_PREFIX)) {
+      appId = StringUtils.substringBetween(servletPath, URL_CONFIGFILES_RAW_PREFIX, URL_SEPARATOR);
     } else if (StringUtils.startsWith(servletPath, URL_CONFIGFILES_PREFIX)) {
       appId = StringUtils.substringBetween(servletPath, URL_CONFIGFILES_PREFIX, URL_SEPARATOR);
     } else if (StringUtils.startsWith(servletPath, URL_NOTIFICATIONS_PREFIX)) {
